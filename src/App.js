ddc045a0e27438021a5a25b2a35a612d35ff7912
anoopsd <anoopsdmadathara@gmail.com>
2020-06-10 09:31:21 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-class App extends Component {
-  render() {
-    return (
+const app = (props)=> {
+  const [personsState, setPersonState] = useState({    
+      persons:[
+        {name: 'Anoop', age : 23},
+        {name: 'rejin', age : 22}
+      ]
+  });
+
+  const switchNameHandler = () =>{
+    // console.log('clicked');
+    setPersonState({
+      persons: [
+        {name: 'Anoop SD', age : 23},
+        {name: 'rejin', age : 26}
+      ]
+  
+    });
+  }
+     return (
       <div className="App">
-       
+       <h1>Hi I am a react app</h1>
+       <button onClick = {switchNameHandler}>Click me</button>
+       <Person name={personsState.persons[0].name} age = {personsState.persons[0].age}> dickhead </Person>
+       <Person name={personsState.persons[1].name} age = {personsState.persons[1].age}> dickhead </Person>
       </div>
     );
   }
-}
 
-export default App;
+
+export default app;
+
+
+
